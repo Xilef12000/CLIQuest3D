7bf84302aa8482c866660ab56eb7ba34d619502f
alavanou <niklas.bachmann@posteo.de>
2023-12-30 09:07:25 +0100

    functions raycast draw3d

--- src/main.c
+++ src/main.c
@@ -9,6 +9,8 @@
 
 int map(int x, int inMin, int inMax, int outMin, int outMax);
 void kb_control();
+void ray_cast();
+void draw_3d();
 
 #define WORLDSIZE 20
 struct level {
@@ -45,6 +47,8 @@ struct level lvl = { // current level lvl
     180
 };
 
+struct buffer fb;
+
 // menu
 unsigned short isMenu = 1;
 int loop = 1; // loop until exit
@@ -69,6 +73,7 @@ const float stepXY = 0.5; // player step size
 const int stepA = 5;  // player rotation steps
 int cliX = 144; // default fallback windows size 
 int cliY = 48;
+double cliA;
 
 int main(int argc, char const *argv[]) { 
     // output character dictionary  
@@ -76,7 +81,6 @@ int main(int argc, char const *argv[]) {
     struct dict codes[CODESLEN];
     setCodes(&codes[0], CODESLEN);
     // init buffer
-    struct buffer fb;
     fb.sY = malloc(sizeof(unsigned short));
     fb.sX = malloc(sizeof(unsigned short));
     // get window size in characters
@@ -84,7 +88,7 @@ int main(int argc, char const *argv[]) {
         cliY = (*fb.sY) - 4;
         cliX = (*fb.sX) - 4;
     }
-    double cliA = fov / (double) cliX;
+    cliA = fov / (double) cliX;
     // clear buffer
     fb.bP = malloc(sizeof(unsigned short)*(*fb.sX)*(*fb.sY));
     fb.cur = malloc(sizeof(fb.cur));
@@ -105,98 +109,11 @@ int main(int argc, char const *argv[]) {
     while(loop) {
         while(getKeysInBuffer()) kb_control(); // inBuffer > 0
         if (!isMenu){
-        // ray casting:
-        // draw lines from player in every direction in view
-        // lines can only be drawn between between to points
-        // point one: player, and point two a pointer outside of world so it can never be reached
-        // the line is drawn in small steps from the first to the second point
-        for (int i = 0; i < cliX; i++) {
-            float l = 0; // line step counter
-            float x1 = pY; // point one = player position
-            float y1 = pX;
-            // line degree in radiens (looking direction - half pov + line offset)
-            float srad = (((pA - (double) fov / 2) + (i * cliA)) * M_PI) / 180;
-            float y2 = y1+(sin(srad)*WORLDSIZE+1); // calculate point two
-            float x2 = x1+(cos(srad)*WORLDSIZE+1);
+            ray_cast();
+            fprintB(fb, "X:%6.3f/Y:%6.3f/% 4d° \r\n", pX, pY, pA); // print player coordinates and rotation
+            setBCur(0, 1, fb);
 
-            float dx =  fabs(x2 - x1); // delta between x1 and x2
-            float sx = x1<x2 ? 0.1 : -0.1; // direction of x delta -> x step
-            float dy = -fabs(y2 - y1); // delta between y1 and y2
-            float sy = y1<y2 ? 0.1 : -0.1; // direction of y delta -> y step
-            float err = dx+dy, e2;
-            while (1) {
-                // if current step position in wall
-                if (lvl.world[(int)x1][(int)y1] == 1) {
-                    distance[i] = l/10; // safe distance to wall (number of necessary steps)
-                    break; // exit to next view line
-                }
-                l++; // next step
-                e2 = 2*err;
-                // determine if this step should be done on x or y axis -> increase error (difference to ideal line) and make step 
-                if (e2 >= dy) {
-                    err += dy; 
-                    x1 += sx;
-                }
-                else if (e2 <= dx) {
-                    err += dx; 
-                    y1 += sy;
-                }   
-            }
-        }
-        fprintB(fb, "X:%6.3f/Y:%6.3f/% 4d° \r\n", pX, pY, pA); // print player coordinates and rotation
-        setBCur(0, 1, fb);
-        for (int i = 0; i < cliY; i++) { //for every horizontal line of output image
-            for (int j = cliX-1; j >= 0; j--) { // for every pixel in horizontal line
-                if (cliY/2-(cliY-map(distance[j],0,maxVDist,0,cliY))/2 <= i) { // if pixel is not ceiling -> wall or floor
-                    if (cliY/2+(cliY-map(distance[j],0,maxVDist,0,cliY))/2 >= i) { // if pixel is not floor -> wall
-                        // draw gray shade depending on distance of wall to player
-                        if (distance[j] < maxVDist*0.25){
-                           putB(11000, fb);
-                        }
-                        else if (distance[j] < maxVDist*0.5){
-                           putB(11001, fb);
-                        }
-                        else if (distance[j] < maxVDist*0.75){
-                           putB(11002, fb);
-                        }
-                        else if (distance[j] < maxVDist){
-                           putB(11003, fb);
-                        }   
-                        else {
-                            putB(11004, fb);
-                        }
-                    }
-                    else { // -> pixel is floor
-                        //  draw gray shade depending on distance of floor to player
-                        if (i < cliY/2/7*1){
-                           putB(12000, fb);
-                        }
-                        else if (i < cliY/2+cliY/2/7*2){
-                           putB(12001, fb);
-                        }
-                        else if (i < cliY/2+cliY/2/7*3){
-                           putB(12002, fb);
-                        }
-                        else if (i < cliY/2+cliY/2/7*4){
-                           putB(12003, fb);
-                        }
-                        else if (i < cliY/2+cliY/2/7*5){
-                           putB(12004, fb);
-                        }
-                        else if (i < cliY/2+cliY/2/7*6){
-                           putB(12005, fb);
-                        }
-                        else {
-                           putB(12006, fb);
-                        }
-                    }
-                }
-                else { // -> pixel is ceiling
-                   putB(' ', fb); // empty pixel
-                }
-            }
-            putB('\n', fb); // cursor to new line after end of horizontal line 
-        } 
+            draw_3d();
         // draw map
         setBCur(0, 1, fb); //reset cursor to top left corner
         short mY1 = pY-(float)mapS/2;
@@ -350,3 +267,101 @@ void kb_control()
         pY = pNY;
     }
 }
+void ray_cast(){
+    // ray casting:
+    // draw lines from player in every direction in view
+    // lines can only be drawn between between to points
+    // point one: player, and point two a pointer outside of world so it can never be reached
+    // the line is drawn in small steps from the first to the second point
+    for (int i = 0; i < cliX; i++) {
+        float l = 0; // line step counter
+        float x1 = pY; // point one = player position
+        float y1 = pX;
+        // line degree in radiens (looking direction - half pov + line offset)
+        float srad = (((pA - (double) fov / 2) + (i * cliA)) * M_PI) / 180;
+        float y2 = y1+(sin(srad)*WORLDSIZE+1); // calculate point two
+        float x2 = x1+(cos(srad)*WORLDSIZE+1);
+
+        float dx =  fabs(x2 - x1); // delta between x1 and x2
+        float sx = x1<x2 ? 0.1 : -0.1; // direction of x delta -> x step
+        float dy = -fabs(y2 - y1); // delta between y1 and y2
+        float sy = y1<y2 ? 0.1 : -0.1; // direction of y delta -> y step
+        float err = dx+dy, e2;
+        while (1) {
+            // if current step position in wall
+            if (lvl.world[(int)x1][(int)y1] == 1) {
+                distance[i] = l/10; // safe distance to wall (number of necessary steps)
+                break; // exit to next view line
+            }
+            l++; // next step
+            e2 = 2*err;
+            // determine if this step should be done on x or y axis -> increase error (difference to ideal line) and make step 
+            if (e2 >= dy) {
+                err += dy; 
+                x1 += sx;
+            }
+            else if (e2 <= dx) {
+                err += dx; 
+                y1 += sy;
+            }   
+        }
+    }
+}
+
+void draw_3d()
+{
+    for (int i = 0; i < cliY; i++) { //for every horizontal line of output image
+        for (int j = cliX-1; j >= 0; j--) { // for every pixel in horizontal line
+            if (cliY/2-(cliY-map(distance[j],0,maxVDist,0,cliY))/2 <= i) { // if pixel is not ceiling -> wall or floor
+                if (cliY/2+(cliY-map(distance[j],0,maxVDist,0,cliY))/2 >= i) { // if pixel is not floor -> wall
+                    // draw gray shade depending on distance of wall to player
+                    if (distance[j] < maxVDist*0.25){
+                        putB(11000, fb);
+                    }
+                    else if (distance[j] < maxVDist*0.5){
+                        putB(11001, fb);
+                    }
+                    else if (distance[j] < maxVDist*0.75){
+                        putB(11002, fb);
+                    }
+                    else if (distance[j] < maxVDist){
+                        putB(11003, fb);
+                    }   
+                    else {
+                        putB(11004, fb);
+                    }
+                }
+                else { // -> pixel is floor
+                    //  draw gray shade depending on distance of floor to player
+                    if (i < cliY/2/7*1){
+                        putB(12000, fb);
+                    }
+                    else if (i < cliY/2+cliY/2/7*2){
+                        putB(12001, fb);
+                    }
+                    else if (i < cliY/2+cliY/2/7*3){
+                        putB(12002, fb);
+                    }
+                    else if (i < cliY/2+cliY/2/7*4){
+                        putB(12003, fb);
+                    }
+                    else if (i < cliY/2+cliY/2/7*5){
+                        putB(12004, fb);
+                    }
+                    else if (i < cliY/2+cliY/2/7*6){
+                        putB(12005, fb);
+                    }
+                    else {
+                        putB(12006, fb);
+                    }
+                }
+            }
+            else { // -> pixel is ceiling
+                putB(' ', fb); // empty pixel
+            }
+        }
+        putB('\n', fb); // cursor to new line after end of horizontal line 
+    } 
+}
+
+
