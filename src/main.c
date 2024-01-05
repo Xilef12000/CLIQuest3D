ad1bda86514e632d860a6ecf0b992ac6a6726fe5
alavanou <niklas.bachmann@posteo.de>
2024-01-05 11:07:03 +0100

    raycast to draw.c

--- src/main.c
+++ src/main.c
@@ -59,45 +59,5 @@ int main(int argc, char const *argv[]) {
 
 
 
-void ray_cast(struct position player_loc, unsigned short *distance){
-    // ray casting:
-    // draw lines from player_loc in every direction in view
-    // lines can only be drawn between between to points
-    // point one: player_loc, and point two a pointer outside of world so it can never be reached
-    // the line is drawn in small steps from the first to the second point
-    for (int i = 0; i < cliX; i++) {
-        float l = 0; // line step counter
-        float x1 = player_loc.pY; // point one = player_loc position
-        float y1 = player_loc.pX;
-        // line degree in radiens (looking direction - half pov + line offset)
-        float srad = (((player_loc.pA - (double) fov / 2) + (i * cliA)) * M_PI) / 180;
-        float y2 = y1+(sin(srad)*WORLDSIZE+1); // calculate point two
-        float x2 = x1+(cos(srad)*WORLDSIZE+1);
-
-        float dx =  fabs(x2 - x1); // delta between x1 and x2
-        float sx = x1<x2 ? 0.1 : -0.1; // direction of x delta -> x step
-        float dy = -fabs(y2 - y1); // delta between y1 and y2
-        float sy = y1<y2 ? 0.1 : -0.1; // direction of y delta -> y step
-        float err = dx+dy, e2;
-        while (1) {
-            // if current step position in wall
-            if (lvl.world[(int)x1][(int)y1] == 1) {
-                distance[i] = l/10; // safe distance to wall (number of necessary steps)
-                break; // exit to next view line
-            }
-            l++; // next step
-            e2 = 2*err;
-            // determine if this step should be done on x or y axis -> increase error (difference to ideal line) and make step 
-            if (e2 >= dy) {
-                err += dy; 
-                x1 += sx;
-            }
-            else if (e2 <= dx) {
-                err += dx; 
-                y1 += sy;
-            }   
-        }
-    }
-}
 
 
