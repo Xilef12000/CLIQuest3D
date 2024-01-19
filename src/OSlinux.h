c34d7e9af512d8650069870ff0a44e1fd751a3b1
alavanou <niklas.bachmann@posteo.de>
2024-01-19 14:55:07 +0100

    OS.h comments

--- src/OSlinux.h
+++ src/OSlinux.h
@@ -7,11 +7,13 @@
 
     #define UNILEN 26
     struct dict codes[UNILEN] = {
+        // wall
         {11000, "\u2588"},
         {11001, "\u2593"},
         {11002, "\u2592"},
         {11003, "\u2591"},
         {11004, " "},
+        // floor
         {12000, "\u2581"},
         {12001, "\u2582"},
         {12002, "\u2583"},
@@ -19,8 +21,11 @@
         {12004, "\u2585"},
         {12005, "\u2586"},
         {12006, "\u2587"},
+
         {12007, "\u259A"}, //wall goal
         {12008, "\u25D9"},  // wall removable
+
+        // minimap
         {13000, "\u2191"},
         {13001, "\u2196"},
         {13002, "\u2190"},
@@ -29,16 +34,24 @@
         {13005, "\u2198"},
         {13006, "\u2192"},
         {13007, "\u2197"},
+
+        // shoot animation
         {14000, "*"},
         {14001, "#"},
         {14002, "^"},
         {14003, "$"},
     };
 
+    // set console options for output cursor visibility an codepage
     int initOS();
-    int getCliDim(int *cliX, int *cliY);
-    int getKeysInBuffer();
-    char getKey();
 
+    // get dimensions of console in charaters
+    int getCliDim(int *cliX, int *cliY);
+
+    // determine if chars are left from keyboard input
+    int getKeysInBuffer();
+
+    // get single key from input buffer
+    char getKey();
 
 #endif // OSLINUX_H_
--- src/OSwin.h
+++ src/OSwin.h
@@ -7,11 +7,13 @@
 
     #define UNILEN 26
     struct dict codes[UNILEN] = {
+        // wall 
         {11000, "\u2588"},
         {11001, "\u2593"},
         {11002, "\u2592"},
         {11003, "\u2591"},
         {11004, " "},
+        // floor
         {12000, "\x2E"},
         {12001, "\x2D"},
         {12002, "\x2D"},
@@ -19,8 +21,10 @@
         {12004, "\x3D"},
         {12005, "\u2261"},
         {12006, "\u2261"},
+
         {12007, "\u25D9"}, //wall goal
         {12008, "\u2592"},  // wall removable
+        // minimap
         {13000, "\u2191"},
         {13001, "\u2196"},
         {13002, "\u2190"},
@@ -29,15 +33,23 @@
         {13005, "\u2198"},
         {13006, "\u2192"},
         {13007, "\u2197"},
+        // shoot animation
         {14000, "*"},
         {14001, "#"},
         {14002, "^"},
         {14003, "$"},
     };
 
+    // set console options for output cursor visibility an codepage
     int initOS();
+
+    // get dimensions of console in charaters
     int getCliDim(int *cliX, int *cliY);
+
+    // determine if chars are left from keyboard input
     int getKeysInBuffer();
+
+    // get single key from input buffer
     char getKey();
 
 #endif // OSWIN_H_
